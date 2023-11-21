6fc8d9d55b4db781229fe5501e16d4b93a1c53c6
Alexander Holmes <rulo@Alexanders-MBP.home>
2023-11-21 17:50:40 +0100

    updated sections

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const introText = document.getElementById("intro-text");
     const subheader = document.getElementById("subheader-text");
+    const subheader2 = document.getElementById("subheader-text-2");
     const socialIcons = document.querySelector(".social-icons");
+    const downArrow = document.getElementById("down-arrow");
 
-    const typewriterEffect = async (element, text, speed = 100) => {
+    const typewriterEffect = async (element, text, speed = 50) => {
         for (const char of text) {
             await new Promise(resolve => setTimeout(() => {
                 element.textContent += char;
@@ -12,8 +14,9 @@ document.addEventListener("DOMContentLoaded", async function () {
         }
     };
 
+    // Fade in icons and intro arrow
     const fadeInIcons = async () => {
-        await new Promise(resolve => setTimeout(resolve, 500)); // Wait for 500ms after typing
+        await new Promise(resolve => setTimeout(resolve, 100)); // Wait for 500ms after typing
 
         // Show the social icons with a fade-in effect
         socialIcons.style.visibility = "visible";
@@ -23,13 +26,29 @@ document.addEventListener("DOMContentLoaded", async function () {
         socialIcons.style.opacity = 1;
     };
 
+    const fadeInArrow = async () => {
+        await new Promise(resolve => setTimeout(resolve, 100)); // Wait for 500ms after typing
+
+        // Show the arrow with a fade-in effect
+        downArrow.style.visibility = "visible";
+        downArrow.style.opacity = 0;
+
+        downArrow.style.transition = "opacity 2s";
+        downArrow.style.opacity = 1;
+    };
+
     const text1 = introText.textContent;
     introText.textContent = "";
 
     const text2 = subheader.textContent;
     subheader.textContent = "";
 
+    const text3 = subheader2.textContent;
+    subheader2.textContent = "";
+
     await typewriterEffect(introText, text1);
     await typewriterEffect(subheader, text2, 50);
+    await typewriterEffect(subheader2, text3, 10);
     await fadeInIcons();
+    await fadeInArrow();
 });
